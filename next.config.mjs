071363d4940d225bb53bfe5d6aff3f32f98ec22c
ANIKET PANWAR <panwaraniket007@gmail.com>
2025-04-25 23:11:32 +0530

    update script for depoloyment on git pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/todo_app",
+  assetPrefix: "/todo_app/",
+};
 
-export default nextConfig;
+module.exports = nextConfig;
